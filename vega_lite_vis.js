cba5e03f81dcb844759adc0df5f203dadd1bbf7b
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-10-08 16:13:24 +0800

    homework week 10 done

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "week9_homework.json";
 vegaEmbed("#homework", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "week10_homework.json";
+vegaEmbed("#homework2", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
